207662d0fdae54fd72af40d9c777b48eb4cfe143
Alberto Kato <albertokatojr@gmail.com>
2022-08-28 19:10:08 -0300

    Added modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EquationComponent } from './components/equation/equation.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { EquationApiService } from './services/equation-api.service';
+import { NumberComponent } from './components/number/number.component';
+import { OperatorComponent } from './components/operator/operator.component';
+import { AnswerComponent } from './components/answer/answer.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EquationComponent
+    EquationComponent,
+    NumberComponent,
+    OperatorComponent,
+    AnswerComponent,
+    ProgressComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     EquationApiService
